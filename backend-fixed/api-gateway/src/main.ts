dc218b55e698efcf878586a650e5b02dde3edc5f
Yram Tetteh-Abotsi <99413491+Asheryram@users.noreply.github.com>
2026-03-26 11:22:56 +0000

    feat: enable CORS for all services with localhost origin

--- backend-fixed/api-gateway/src/main.ts
+++ backend-fixed/api-gateway/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5174',
+    credentials: true, // Set to true if you use cookies or auth headers
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
--- backend-fixed/auth-service/src/main.ts
+++ backend-fixed/auth-service/src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+  app.enableCors({
+    origin: 'http://localhost:5174',
+    credentials: true, // Set to true if you use cookies or auth headers
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.connectMicroservice<MicroserviceOptions>({
