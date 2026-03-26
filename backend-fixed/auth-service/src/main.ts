7f2369f2cf0df3ef5afa9bfc1277025627f03cc1
Yram Tetteh-Abotsi <99413491+Asheryram@users.noreply.github.com>
2026-03-26 12:26:08 +0000

    feat: update CORS configuration for all services to support multiple origins and HTTP methods

--- backend-fixed/auth-service/src/main.ts
+++ backend-fixed/auth-service/src/main.ts
@@ -6,14 +6,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-<<<<<<< HEAD
 
   app.enableCors({
-    origin: 'http://localhost:5174',
-    credentials: true, // Set to true if you use cookies or auth headers
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://2e57-196-61-44-164.ngrok-free.app',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
-=======
->>>>>>> main
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.connectMicroservice<MicroserviceOptions>({
